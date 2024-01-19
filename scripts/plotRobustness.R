ad232105d0734459e9e8fa33b77d337f76a28f2e
AroArz <aron_arzoomand@live.se>
2024-01-19 01:32:20 +0100

    missed lint

--- scripts/plotRobustness.R
+++ scripts/plotRobustness.R
@@ -218,9 +218,9 @@ plot_subjectassignments_with_meanTraj <- function(inputrds, topSeeds, kmlSeedsDe
     
         scale_color_gradient2(
           name="log2(ODDS RATIO) \nCAPPED AT -2 & 2",
-          low = microshades_palettes$micro_green[4], 
+          low = "#41ab5d", 
           mid = "white", # This color is at the midpoint, which is 1.5 in this case.
-          high = microshades_palettes$micro_orange[4], 
+          high = "#fe9929", 
           midpoint = 0, limits = c(-2, 2), na.value = "red",
           space = "Lab",
           guide = "colourbar"
